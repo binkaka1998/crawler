9a960ee206489d00f8bda4e29041e60157c79ce8
Jayce <datht@hivelab.com>
2026-01-29 08:28:19 +0700

    Update/crawler

--- src/crawlers/price/BTMCCrawler.ts
+++ src/crawlers/price/BTMCCrawler.ts
@@ -36,7 +36,7 @@ export class BTMCCrawler {
         let collected = 0;
 
         for (const item of rows) {
-            if (collected >= 7) break;
+            if (collected >= 8) break;
 
             const row = Number(item['@row']);
             if (!row) continue;
